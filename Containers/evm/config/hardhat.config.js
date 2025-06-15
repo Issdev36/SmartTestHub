05be9dd0962fc525727e923cd9ca021d1e063bb8
Jeffery Akorful <149057144+AduAkorful@users.noreply.github.com>
2025-06-15 20:24:56 +0000

    Update hardhat.config.js

--- Containers/evm/config/hardhat.config.js
+++ Containers/evm/config/hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
   solidity: {
@@ -24,14 +25,37 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545",
       chainId: 1337
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     }
   },
   gasReporter: {
-    enabled: true,
+    enabled: process.env.REPORT_GAS ? true : false,
     currency: "USD",
     outputFile: "logs/gas-report.txt",
     noColors: true,
-    coinmarketcap: process.env.COINMARKETCAP_API_KEY || ""
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY || "",
+    gasPrice: 20,
+    token: "ETH",
+    showTimeSpent: true,
+    excludeContracts: ["Migrations", "Mock", "Test"]
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      goerli: process.env.ETHERSCAN_API_KEY,
+      sepolia: process.env.ETHERSCAN_API_KEY,
+    }
   },
   mocha: {
     timeout: 20000
